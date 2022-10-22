53cffd7c9358efb7447930a371e92c217f8b4c9a
Esteban Escalante <86449094+Esca01@users.noreply.github.com>
2022-10-22 22:43:20 +0000

    Initial commit

---
+++ prueba.php
@@ -0,0 +1,10 @@
+<?php if (isset($_REQUEST['name'])){
+              $name = $_REQUEST['name'];
+               $msg = "Hello, " . $name . "!";
+               $encoded = htmlspecialchars($msg); 
+}
+// Esteban
+# Esteban
+/* esteban */
+  
+?>
